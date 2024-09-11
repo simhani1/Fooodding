88a08a59614c8af1244957df6312ff2a30c467e0
심종한 <simhani1@gmail.com>
2024-09-11 17:52:27 +0900

    test: 푸드트럭 등록 service 단위 테스트 추가

--- backend/api/src/main/java/com/fooding/api/foodtruck/domain/FoodCategory.java
+++ backend/api/src/main/java/com/fooding/api/foodtruck/domain/FoodCategory.java
@@ -1,6 +1,20 @@
 package com.fooding.api.foodtruck.domain;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum FoodCategory {
 
+	KOREAN("한식"),
+	JAPANESE("양식"),
+	CHINESE("중식"),
+	WESTERN("양식"),
+	BUNSIK("분식"),
+	ASIAN("아시안"),
+	FAST_FOOD("패스트푸드"),
+	CAFE_DESSERT("카페/디저트"),
+	;
+
+	private String name;
 
 }
---
+++ backend/api/src/test/java/com/fooding/api/foodtruck/service/RegisterFoodTruckServiceImplTest.java
@@ -0,0 +1,68 @@
+package com.fooding.api.foodtruck.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.fooding.api.foodtruck.domain.FoodTruck;
+import com.fooding.api.foodtruck.repository.FoodTruckRepository;
+import com.fooding.api.foodtruck.service.dto.FoodTruckDto;
+import com.fooding.api.owner.domain.Owner;
+import com.fooding.api.owner.domain.Provider;
+import com.fooding.api.owner.repository.OwnerRepository;
+
+@ExtendWith(MockitoExtension.class)
+class RegisterFoodTruckServiceImplTest {
+
+	@InjectMocks
+	private RegisterFoodTruckServiceImpl registerFoodTruckServiceImpl;
+
+	@Mock
+	private FoodTruckRepository foodTruckRepository;
+
+	@Mock
+	private OwnerRepository ownerRepository;
+
+	@DisplayName("푸드트럭 등록에 성공한다.")
+	@Test
+	void registerFoodTruckSuccess() {
+		// given
+		Long ownerId = 1L;
+
+		Owner owner = Owner.builder()
+			.ages("20-30")
+			.gender("MALE")
+			.nickname("nickname")
+			.provider(Provider.NAVER)
+			.build();
+
+		FoodTruckDto dto = FoodTruckDto.builder()
+			.name("name")
+			.snsLink("snsLink")
+			.licenseNumber("licenseNumber")
+			.introduction("introduction")
+			.category("KOREAN")
+			.bank("bank")
+			.accountName("accountName")
+			.accountNumber("accountNumber")
+			.build();
+
+		// when
+		when(ownerRepository.findById(anyLong())).thenReturn(Optional.of(owner));
+		when(foodTruckRepository.save(any(FoodTruck.class))).thenReturn(any(FoodTruck.class));
+
+		// then
+		assertDoesNotThrow(() -> registerFoodTruckServiceImpl.registerFoodTruck(ownerId, dto));
+		verify(foodTruckRepository, times(1)).save(any(FoodTruck.class));
+	}
+
+}

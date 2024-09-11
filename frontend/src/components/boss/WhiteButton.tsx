e34f490fd052253c89dde9eeab10d0f1196d895e
김윤 <cho_lyn@naver.com>
2024-09-11 12:06:02 +0900

    feat: 사장님 지도 플로팅 버튼 구현 - 화이트 ver.

---
+++ frontend/src/components/boss/WhiteButton.tsx
@@ -0,0 +1,12 @@
+const WhiteButton = () => {
+	return (
+		<div
+			id="white-button"
+			className="w-56 p-4 absolute bottom-8 left-8 rounded-lg bg-white border border-solid border-gray shadow-sm"
+		>
+			<p className="text-3xl text-gray font-bold text-center">서울 전체 보기</p>
+		</div>
+	);
+};
+
+export default WhiteButton;

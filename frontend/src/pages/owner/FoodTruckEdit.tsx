bf64ce79f56fd86a45f337816440f829eb76299f
김예훈 <rladpgns00@gmail.com>
2024-09-25 14:30:58 +0900

    feat: swagger 삭제 및 관련된 설정 수정 / 푸드트럭 폼 컴포넌트 리팩토링

--- frontend/src/pages/owner/FoodTruckEdit.tsx
+++ frontend/src/pages/owner/FoodTruckEdit.tsx
@@ -1,34 +1,15 @@
-import { useNavigate } from "react-router-dom";
+import FoodTruckFormContainer from "@components/owner/FoodTruckFormContainer";
 
-import Title from "@components/common/Title";
-import FoodTruckForm from "@components/owner/FoodTruckForm";
-import Container from "@components/owner/Container";
-import Main from "@components/owner/Main";
-
-const OwnerEdit = () => {
-	const nav = useNavigate();
+const FoodTruckEdit = () => {
+	const editFoodTruck = () => {};
 
 	return (
-		<Container>
-			<Main>
-				<>
-					<div className="flex justify-between">
-						<Title title="푸드트럭 정보 수정" />
-						<button
-							className="text-3xl"
-							onClick={() => nav("/owner/foodtruck")}
-						>
-							취소
-						</button>
-					</div>
-					<FoodTruckForm
-						buttonText="수정 완료"
-						onSubmit={() => {}}
-					/>
-				</>
-			</Main>
-		</Container>
+		<FoodTruckFormContainer
+			title="푸드트럭 정보 수정"
+			buttonText="수정 완료"
+			onSubmit={editFoodTruck}
+		/>
 	);
 };
 
-export default OwnerEdit;
+export default FoodTruckEdit;
--- frontend/src/pages/owner/OwnerFoodTruck.tsx
+++ frontend/src/pages/owner/OwnerFoodTruck.tsx
@@ -9,8 +9,7 @@ import useFoodTruckStore from "@store/foodTruckStore";
 
 const OwnerFoodTruck = () => {
 	const nav = useNavigate();
-	const { name, licenseNumber, introduction, category } = useFoodTruckStore();
-	const isExist = name && licenseNumber && category;
+	const { name, licenseNumber, introduction, category, isExist } = useFoodTruckStore();
 
 	return (
 		<Container>
@@ -46,9 +45,9 @@ const OwnerFoodTruck = () => {
 				</Main>
 			) : (
 				<div className="flex flex-col justify-center h-screen gap-10">
-					<h4 className="text-center text-3xl">아직 푸드트럭을 등록하지 않았어요.</h4>
+					<h4 className="text-3xl text-center">아직 푸드트럭을 등록하지 않았어요.</h4>
 					<button
-						className="w-96 h-16 mx-auto text-2xl bg-gradient-to-b from-main to-boss text-white rounded-xl"
+						className="h-16 mx-auto text-2xl text-white w-96 bg-gradient-to-b from-main to-boss rounded-xl"
 						onClick={() => nav("/owner/foodtruck/create")}
 					>
 						푸드트럭 등록하기

2702a12d066bc20c1c154135803e1cb05e8808d3
김예훈 <rladpgns00@gmail.com>
2024-09-29 20:18:54 +0900

    feat: 푸드트럭 정보 수정 API 연결

--- frontend/src/pages/owner/FoodTruckEdit.tsx
+++ frontend/src/pages/owner/FoodTruckEdit.tsx
@@ -1,12 +1,44 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import FoodTruckFormContainer from "@components/owner/FoodTruckFormContainer";
 
+import { IFoodTruckDTO } from "@interface/api";
+import { updateFoodTruck } from "@api/food-truck-api";
+import useFoodTruckStore from "@store/foodTruckStore";
+
 const FoodTruckEdit = () => {
-	const editFoodTruck = () => {};
+	const nav = useNavigate();
+
+	const { name, licenseNumber, introduction, category, foodTruckId, updateAll } = useFoodTruckStore();
+
+	const [formData, setFormData] = useState<IFoodTruckDTO>({
+		name,
+		licenseNumber,
+		introduction,
+		category,
+	});
+
+	const editFoodTruck = async () => {
+		try {
+			const { data } = await updateFoodTruck(foodTruckId, {
+				...formData,
+			});
+			if (data.isSuccess) {
+				updateAll({ ...formData, foodTruckId });
+			}
+		} catch (error) {
+		} finally {
+			nav("/owner/foodtruck");
+		}
+	};
 
 	return (
 		<FoodTruckFormContainer
 			title="푸드트럭 정보 수정"
 			buttonText="수정 완료"
+			formData={formData}
+			setFormData={setFormData}
 			onSubmit={editFoodTruck}
 		/>
 	);

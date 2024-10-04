e054de73686ba4ecf577c345e2d52f46dc9b1fd7
kbs <bumsoo0703@naver.com>
2024-10-04 20:45:12 +0900

    fix: json파일 env형식으로 변환

--- backend/api/src/main/java/com/fooding/api/core/config/fcm/FcmConfig.java
+++ backend/api/src/main/java/com/fooding/api/core/config/fcm/FcmConfig.java
@@ -1,37 +1,42 @@
-// package com.fooding.api.core.config.fcm;
-//
-// import java.io.IOException;
-// import java.io.InputStream;
-//
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.core.io.ClassPathResource;
-// import org.springframework.stereotype.Service;
-//
-// import com.google.auth.oauth2.GoogleCredentials;
-// import com.google.firebase.FirebaseApp;
-// import com.google.firebase.FirebaseOptions;
-//
-// import jakarta.annotation.PostConstruct;
-//
-// @Service
-// public class FcmConfig {
-//
-// 	 @Value("${fcm-service-key}")
-// 	 private String FCM_CONFIG_PATH;
-//
-// 	@PostConstruct
-// 	public void initialize() throws IOException {
-// 		ClassPathResource resource = new ClassPathResource(FCM_CONFIG_PATH);
-//
-// 		try (InputStream serviceAccount = resource.getInputStream()) {
-// 			FirebaseOptions options = FirebaseOptions.builder()
-// 				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-// 				.build();
-//
-// 			if (FirebaseApp.getApps().isEmpty()) {
-// 				FirebaseApp.initializeApp(options);
-// 			}
-// 		}
-// 	}
-//
-// }
+package com.fooding.api.core.config.fcm;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
+public class FcmConfig {
+
+	@Value("${FCM_PRIVATE_KEY}")
+	private String privateKey;
+	@Value("${FCM_CLIENT_EMAIL}")
+	private String clientEmail;
+	@Value("${FCM_PROJECT_ID}")
+	private String projectId;
+
+	@PostConstruct
+	public void initialize() throws IOException {
+		String credentialsJson = String.format(
+			"{\"type\":\"service_account\",\"project_id\":\"%s\",\"private_key\":\"%s\",\"client_email\":\"%s\"}",
+			projectId, privateKey.replace("\\n", "\n"), clientEmail);
+
+		InputStream serviceAccount = new ByteArrayInputStream(credentialsJson.getBytes());
+
+		FirebaseOptions options = FirebaseOptions.builder()
+			.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+			.build();
+
+		if (FirebaseApp.getApps().isEmpty()) {
+			FirebaseApp.initializeApp(options);
+		}
+	}
+}

dd7bc10525e1b3f990ca4d9710329b0746f10370
오유진 <oyj5775@naver.com>
2024-10-11 02:15:53 +0900

    feat: 진행속도 출력문 추가

--- data/app/routes/target.py
+++ data/app/routes/target.py
@@ -24,6 +24,7 @@ def target():
 
     # DB 연결
     connection = create_connection()
+    print_progress("DB 연결 성공")
 
     # 푸드트럭 카테고리 조회
     category = get_foodtruck_category(connection, foodtruck_id)
@@ -39,6 +40,7 @@ def target():
 
     print_progress("Dask 클라이언트 설정 중...")
     client = Client(n_workers=4, threads_per_worker=2, memory_limit='4GB')
+    print_progress("Dask 클라이언트 설정 완료")
 
     # 행정동코드 가져오기
     행정동코드 = request_data.get('행정동코드')
@@ -46,15 +48,18 @@ def target():
         return jsonify({"error": "행정동 코드를 제공해주세요."}), 400
 
     today = datetime.datetime.now().date()
-    
+    print_progress(f"오늘 날짜: {today}")
+
     # DB 조회 및 예측
     with connection.cursor(dictionary=True) as cursor:
         cursor.execute("SELECT time, predict_people FROM target WHERE location_code = %s AND date = %s", (행정동코드, today))
         existing_predictions = cursor.fetchall()
         if existing_predictions:
+            print_progress("DB에서 예측 결과를 반환합니다.")
             return jsonify({"message": "DB에서 예측 결과를 반환합니다.", "predictions": existing_predictions}), 200
 
     # 데이터 읽기
+    print_progress("데이터 파일 읽는 중...")
     folder_paths = [
          "/app/data/생활이동_행정동_202310/*.csv",
          "/app/data/생활이동_행정동_202210/*.csv",
@@ -69,26 +74,33 @@ def target():
     }
 
     ddf = dd.read_csv(folder_paths, encoding='cp949', dtype=dtypes, blocksize="32MB")
+    print_progress("데이터 파일 읽기 완료")
 
     # 전처리
+    print_progress("데이터 전처리 중...")
     ddf = ddf.map_partitions(preprocess)
     aggregated_data = aggregate_data(ddf, 행정동코드).compute()
+    print_progress("데이터 전처리 완료")
 
     if aggregated_data.empty:
         return jsonify({"error": "해당 행정동코드에 대한 데이터가 없습니다."}), 404
 
     # 성별과 나이에 해당하는 데이터 필터링
     filtered_data = aggregated_data[(aggregated_data['성별'] == gender) & (aggregated_data['연령대'] == age)]
+    print_progress(f"성별: {gender}, 연령대: {age}에 맞는 데이터 필터링 완료")
 
     if filtered_data.empty:
         return jsonify({"error": "성별 및 연령대에 맞는 데이터가 없습니다."}), 404
 
     # 모델 훈련 및 예측
+    print_progress("모델 훈련 중...")
     X = filtered_data[['연도', '월', '요일_숫자', '시간', '오전_오후', '러시아워', '주말']]
     y = filtered_data['이동인구(합)']
     model, scaler = train_model(X, y)
+    print_progress("모델 훈련 완료")
 
     # 예측 수행
+    print_progress("예측 수행 중...")
     hours = list(range(24))
     X_pred = pd.DataFrame({
         '연도': [today.year] * 24,
@@ -105,5 +117,6 @@ def target():
 
     results = [{"시간": hour, "예측 유동인구": max(0, int(pred) / 4)} for hour, pred in zip(hours, predictions)]
     save_target_to_db(connection, 행정동코드, today, results)
+    print_progress("예측 결과 DB에 저장 완료")
 
     return jsonify({"message": "오늘의 예측을 반환합니다.", "predictions": results}), 200
